effe696b010383f174c59cd785177be195f61cd3
Emma <emmalivne.03@gmail.com>
2023-01-11 19:48:05 +0200

    Add LimelightLogInputs subsystem

---
+++ src/main/java/frc/robot/subsystems/vision/LimelightLogInputs.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems.vision;
+
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
+
+public class LimelightLogInputs implements LoggableInputs {
+    public static LimelightLogInputs INSTANCE = null;
+    public double targetDistance;
+    public double yaw;
+    public boolean hasTargets;
+    public float tagId;
+
+    public LimelightLogInputs(){}
+
+    public static LimelightLogInputs getInstance(){
+        if (INSTANCE==null){
+            INSTANCE = new LimelightLogInputs();
+        }
+        return INSTANCE;
+    }
+
+    public void toLog(LogTable table){
+        table.put("targetDistance", targetDistance);
+        table.put("yaw", yaw);
+        table.put("hasTargets", hasTargets);
+        table.put("tagId", tagId);
+    }
+
+    public void fromLog(LogTable table){
+        targetDistance = table.getDouble("targetDistance", targetDistance);
+        yaw = table.getDouble("yaw", yaw);
+        hasTargets = table.getBoolean("hasTargets", hasTargets);
+        tagId = table.getFloat("tagId", tagId);
+    }
+}

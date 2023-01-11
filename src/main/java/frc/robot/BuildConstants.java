d96385098bc792d076858561fc64101b75d3d9c3
eitan <eitan.vichik@gmail.com>
2023-01-11 17:00:04 +0200

    Replace navx json with new version
    This one shouldn't throw an exception

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "dc472deb3b67965059f461c6f2f5f91e6629f7ad";
-  public static final String GIT_DATE = "2023-01-11 02:56:16 EST";
+  public static final int GIT_REVISION = 18;
+  public static final String GIT_SHA = "52630e8dec028362b928a5cb5c34382c7559a308";
+  public static final String GIT_DATE = "2023-01-11 05:38:52 EST";
   public static final String GIT_BRANCH = "swerve-drive";
-  public static final String BUILD_DATE = "2023-01-11 03:00:13 EST";
-  public static final long BUILD_UNIX_TIME = 1673424013270L;
+  public static final String BUILD_DATE = "2023-01-11 09:43:07 EST";
+  public static final long BUILD_UNIX_TIME = 1673448187268L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

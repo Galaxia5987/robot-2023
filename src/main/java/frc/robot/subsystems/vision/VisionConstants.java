afdb4151b17dcb20466fa869310a3bafa9cc635b
Emma <emmalivne.03@gmail.com>
2023-01-11 21:41:56 +0200

    Add temporary constants

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -3,4 +3,6 @@ package frc.robot.subsystems.vision;
 public class VisionConstants {
     public static final double CAMERA_HEAIGHT = 0;
     public static final double CAMERA_PITCH = 0;
+    public static final double UPPER_CONE_TARGET11_X_DISTANCE = 0;
+    public static final double UPPER_CONE_TARGET11_Y_DISTANCE = 0;
 }

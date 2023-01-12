e8021709db3270d764753161580a13381058530a
GaiaZ <gaya.zano11@gmail.com>
2023-01-12 17:53:39 +0200

    aaaa

---
+++ src/main/java/frc/robot/subsystems/LimeLightModule/Constants/Constants.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.LimeLightModule.Constants;
+
+public class Constants {
+    private final double XTarget= 0;
+    private final double XTarget= 0;
+    private final double YTarget= 0;
+}

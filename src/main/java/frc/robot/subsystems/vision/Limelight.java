d6655bb3d7cb2d7e59f58f6b33ffaaf8fde95597
Emma <emmalivne.03@gmail.com>
2023-01-12 11:16:17 +0200

    Fix spelling and rename visionConstants

--- src/main/java/frc/robot/subsystems/vision/Limelight.java
+++ src/main/java/frc/robot/subsystems/vision/Limelight.java
@@ -29,7 +29,9 @@ public class Limelight extends LoggedSubsystem<LimelightLogInputs> {
         PortForwarder.add(5801, "limelight.local", 5801);
     }
 
-    public Limelight getINSTANCE() {
+    public Limelight getInstance() {]
+
+        
         if (INSTANCE == null) {
             INSTANCE = new Limelight();
         }
@@ -49,7 +51,7 @@ public class Limelight extends LoggedSubsystem<LimelightLogInputs> {
     }
 
     public OptionalDouble getTargetDistance() {
-        double totalPitch = VisionConstants.CAMERA_PITCH + getPitch().getRadians();
+        double totalPitch = Constants.CAMERA_PITCH + getPitch().getRadians();
         if (hasTargets()) {
             return OptionalDouble.of(tx.getDouble(0) * Math.sin(totalPitch));
         }
@@ -68,8 +70,8 @@ public class Limelight extends LoggedSubsystem<LimelightLogInputs> {
         double xTargetDistance = getTargetDistance().getAsDouble() * Math.sin(absouluteAngle);
         double yTargetDistance = getTargetDistance().getAsDouble() * Math.cos(absouluteAngle);
         if (hasTargets()) {
-            double xDistance = xTargetDistance - VisionConstants.UPPER_CONE_TARGET11_X_DISTANCE;
-            double yDistance = yTargetDistance - VisionConstants.UPPER_CONE_TARGET11_Y_DISTANCE;
+            double xDistance = xTargetDistance - Constants.UPPER_CONE_TARGET11_X_DISTANCE;
+            double yDistance = yTargetDistance - Constants.UPPER_CONE_TARGET11_Y_DISTANCE;
             Translation2d translation2d = new Translation2d(xDistance, yDistance);
             return Optional.of(new Pose2d(translation2d, robotAngle));
         }

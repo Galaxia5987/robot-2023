cea21e11835c773c77f60d643a8e696516a07821
eitan <eitan.vichik@gmail.com>
2023-01-15 09:25:44 +0200

    Revert "Change to jdk 11"
    This reverts commit 77288091c9cf5b8861a16897502304008eb6f2d0.

--- src/main/java/frc/robot/subsystems/drivetrain/commands/BalanceOnStation.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/BalanceOnStation.java
@@ -3,11 +3,13 @@ package frc.robot.subsystems.drivetrain.commands;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.subsystems.drivetrain.DriveSignal;
 import frc.robot.subsystems.drivetrain.SwerveConstants;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.gyroscope.Gyroscope;
+import frc.robot.utils.Utils;
 
 public class BalanceOnStation extends CommandBase {
     private final SwerveDrive swerveDrive = Robot.swerveSubsystem;
@@ -31,8 +33,12 @@ public class BalanceOnStation extends CommandBase {
 
     @Override
     public void execute() {
-        var angles = gyroscope.getAll();
-        double absoluteAngle = angles.getAngle();
+        var angles = gyroscope.getYawPitchRoll();
+        double absoluteAngle = Utils.relativeAnglesToAbsolutePitch(
+                angles.yaw(),
+                angles.pitch(),
+                angles.roll()
+        );
         double vx = controller.calculate(absoluteAngle, 0);
         vx += SwerveConstants.CHARGING_STATION_BALANCE_Kf * Math.signum(vx);
         swerveDrive.drive(new DriveSignal(vx, 0, 0, new Translation2d(), true));

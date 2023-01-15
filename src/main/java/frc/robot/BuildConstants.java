77288091c9cf5b8861a16897502304008eb6f2d0
eitan <eitan.vichik@gmail.com>
2023-01-15 09:25:44 +0200

    Change to jdk 11

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 26;
-  public static final String GIT_SHA = "4adbd5d85892699daa6308c74cddf2e438ba2ac2";
-  public static final String GIT_DATE = "2023-01-13 08:53:14 EST";
+  public static final int GIT_REVISION = 28;
+  public static final String GIT_SHA = "67a618116fbd8a210cd9f2e012c6c4bd497a4e68";
+  public static final String GIT_DATE = "2023-01-15 01:52:29 EST";
   public static final String GIT_BRANCH = "feature/swerve-drive";
-  public static final String BUILD_DATE = "2023-01-14 16:18:18 EST";
-  public static final long BUILD_UNIX_TIME = 1673731098916L;
+  public static final String BUILD_DATE = "2023-01-15 02:03:41 EST";
+  public static final long BUILD_UNIX_TIME = 1673766221279L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/drivetrain/commands/BalanceOnStation.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/BalanceOnStation.java
@@ -3,13 +3,11 @@ package frc.robot.subsystems.drivetrain.commands;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.subsystems.drivetrain.DriveSignal;
 import frc.robot.subsystems.drivetrain.SwerveConstants;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.gyroscope.Gyroscope;
-import frc.robot.utils.Utils;
 
 public class BalanceOnStation extends CommandBase {
     private final SwerveDrive swerveDrive = Robot.swerveSubsystem;
@@ -33,12 +31,8 @@ public class BalanceOnStation extends CommandBase {
 
     @Override
     public void execute() {
-        var angles = gyroscope.getYawPitchRoll();
-        double absoluteAngle = Utils.relativeAnglesToAbsolutePitch(
-                angles.yaw(),
-                angles.pitch(),
-                angles.roll()
-        );
+        var angles = gyroscope.getAll();
+        double absoluteAngle = angles.getAngle();
         double vx = controller.calculate(absoluteAngle, 0);
         vx += SwerveConstants.CHARGING_STATION_BALANCE_Kf * Math.signum(vx);
         swerveDrive.drive(new DriveSignal(vx, 0, 0, new Translation2d(), true));

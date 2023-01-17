446fa6bfc19507c174929263f049b43ae2c1915d
Emma <emmalivne.03@gmail.com>
2023-01-17 20:21:40 +0200

    Add PrototypeArmLogInputs class

---
+++ src/main/java/frc/robot/subsystems/arm/PrototypeArmLogInputs.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.arm;
+
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
+
+public class PrototypeArmLogInputs implements LoggableInputs {
+
+
+    public PrototypeArmLogInputs(){
+
+    }
+
+    public void toLog(LogTable table){
+
+    }
+
+    public void fromLog(LogTable table){
+
+    }
+}

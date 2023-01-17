e6eeed07e1c1cc3c29adec0f158b602681122bf0
Emma <emmalivne.03@gmail.com>
2023-01-17 22:20:19 +0200

    Add prototype arm ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -2,4 +2,11 @@ package frc.robot;
 
 public final class Ports {
 
+    public static class prototypeArmPorts{
+        public static final int SHOULDER_MOTOR = 0;
+        public static final int ELBOW_MOTOR = 0;
+        public static final int SHOULDER_ENCODER = 0;
+        public static final int ELBOW_ENCODER = 0;
+    }
+
 }

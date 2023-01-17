444762723c70eb3389eb5aa709031f5546c7b2d4
Emma <emmalivne.03@gmail.com>
2023-01-17 22:22:49 +0200

    Add getter and setter functions to PrototypeArm

--- src/main/java/frc/robot/subsystems/arm/PrototypeArm.java
+++ src/main/java/frc/robot/subsystems/arm/PrototypeArm.java
@@ -1,38 +1,64 @@
 package frc.robot.subsystems.arm;
 
-import edu.wpi.first.net.PortForwarder;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.sensors.CANCoder;
+import frc.robot.Ports;
 import frc.robot.subsystems.LoggedSubsystem;
+import frc.robot.utils.units.UnitModel;
 
 public class PrototypeArm extends LoggedSubsystem<PrototypeArmLogInputs> {
-    public static PrototypeArm
+    public static final TalonSRX shoulderMotor = new TalonSRX(Ports.prototypeArmPorts.SHOULDER_MOTOR);
+    public static final TalonSRX elbowMotor = new TalonSRX(Ports.prototypeArmPorts.ELBOW_MOTOR);
+    public static final CANCoder shoulderEncoder = new CANCoder(Ports.prototypeArmPorts.SHOULDER_ENCODER);
+    public static final CANCoder elbowEncoder = new CANCoder(Ports.prototypeArmPorts.ELBOW_ENCODER);
+    public static final UnitModel unitModel = new UnitModel(ArmConstants.TICKS_PER_RADIAN);
+    public static PrototypeArm INSTANCE = null;
 
     public PrototypeArm() {
-        super(new PrototypeArmLogInputs());;
+        super(new PrototypeArmLogInputs());
+
+        shoulderMotor.configVoltageCompSaturation(ArmConstants.CONFIG_VOLT_COMP);
+        shoulderMotor.enableVoltageCompensation(ArmConstants.ENABLE_VOLT_COMPANSATION);
+        shoulderMotor.setNeutralMode(NeutralMode.Brake);
+        shoulderMotor.setInverted(ArmConstants.clockWise);
+
+        elbowMotor.configVoltageCompSaturation(ArmConstants.CONFIG_VOLT_COMP);
+        elbowMotor.enableVoltageCompensation(ArmConstants.ENABLE_VOLT_COMPANSATION);
+        elbowMotor.setNeutralMode(NeutralMode.Brake);
+        elbowMotor.setInverted(ArmConstants.clockWise);
     }
 
-    public void setLowerWristPosition(){
-
+    public void setShoulderJointPower(double power) {
+        shoulderMotor.set(TalonSRXControlMode.PercentOutput, power);
     }
 
-    public void setUpperWristPosition(){
-
+    public void setElbowJointPower(double power) {
+        elbowMotor.set(TalonSRXControlMode.PercentOutput, power);
     }
 
-    public void setLowerWristPower(){
-
+    public double getShoulderJointPosition() {
+        return Math.toDegrees(unitModel.toUnits(shoulderEncoder.getPosition()));
     }
 
-    public void setUpperWristPower(){
-
+    public void setShoulderJointPosition(double angle) {
+        shoulderEncoder.setPosition(unitModel.toTicks(Math.toRadians(angle)));
     }
 
-    public double getLower
+    public double getElbowJointPosition() {
+        return Math.toDegrees(unitModel.toUnits(elbowEncoder.getPosition()));
+    }
 
-    public String getSubsystemName(){
+    public void setElbowJointPosition(double angle) {
+        elbowEncoder.setPosition(unitModel.toTicks(Math.toRadians(angle)));
+    }
+
+    public String getSubsystemName() {
         return "PrototypeArm";
     }
 
-    public void updateInputs(){
+    public void updateInputs() {
 
     }
 }

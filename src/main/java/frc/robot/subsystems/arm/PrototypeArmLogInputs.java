0e0b8707dc7251b3cf6c08d485ec2d3762c70d47
Emma <emmalivne.03@gmail.com>
2023-01-17 22:31:20 +0200

    Add toLog and fromLog functions to PrototypeArmLogInputs

--- src/main/java/frc/robot/subsystems/arm/PrototypeArmLogInputs.java
+++ src/main/java/frc/robot/subsystems/arm/PrototypeArmLogInputs.java
@@ -4,17 +4,26 @@ import org.littletonrobotics.junction.LogTable;
 import org.littletonrobotics.junction.inputs.LoggableInputs;
 
 public class PrototypeArmLogInputs implements LoggableInputs {
+    public double elbowAngle = 0;
+    public double shoulderAngle = 0;
+    public double elbowMotorPower = 0;
+    public double shoulderMotorPower = 0;
 
 
-    public PrototypeArmLogInputs(){
-
+    public PrototypeArmLogInputs() {
     }
 
-    public void toLog(LogTable table){
-
+    public void toLog(LogTable table) {
+        table.put("elbowAngle", elbowAngle);
+        table.put("shoulderAngle", shoulderAngle);
+        table.put("elbowMotorPower", elbowMotorPower);
+        table.put("shoulderMotorPower", shoulderMotorPower);
     }
 
-    public void fromLog(LogTable table){
-
+    public void fromLog(LogTable table) {
+        elbowAngle = table.getDouble("elbowAngle", elbowAngle);
+        shoulderAngle = table.getDouble("shoulderAngle", shoulderAngle);
+        elbowMotorPower = table.getDouble("elbowMotorPower", elbowMotorPower);
+        shoulderMotorPower = table.getDouble("shoulderMotorPower", shoulderMotorPower);
     }
 }

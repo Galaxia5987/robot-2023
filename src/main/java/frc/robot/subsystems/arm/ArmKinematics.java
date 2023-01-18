005e3875285e9f68785bd9aea63b352e79d32c56
eitan <eitan.vichik@gmail.com>
2023-01-18 09:29:21 +0200

    Fix kinematics coordinate systems

--- src/main/java/frc/robot/subsystems/arm/ArmKinematics.java
+++ src/main/java/frc/robot/subsystems/arm/ArmKinematics.java
@@ -12,8 +12,9 @@ public class ArmKinematics {
     }
 
     public Translation2d forwardKinematics(double theta1, double theta2) {
-        double x = l1 * Math.cos(theta1) + l2 * Math.cos(theta1 + theta2);
-        double y = l1 * Math.sin(theta1) + l2 * Math.sin(theta1 + theta2);
+        double theta = theta2 + theta1 - Math.PI / 2;
+        double x = l1 * Math.cos(theta1) + l2 * Math.cos(theta1 + theta);
+        double y = l1 * Math.sin(theta1) + l2 * Math.sin(theta1 + theta);
         return new Translation2d(x, y);
     }
 
@@ -21,7 +22,8 @@ public class ArmKinematics {
         double c2 = (x * x + y * y - l1 * l1 - l2 * l2) / (2 * l1 * l2);
         double s2 = Math.sqrt(1 - c2 * c2);
         double theta1 = Math.acos(c2);
-        double theta2 = Math.acos((y * (l1 + l2 * c2) - x * l2 * s2) / (x * x + y * y));
+        double theta = Math.acos((y * (l1 + l2 * c2) - x * l2 * s2) / (x * x + y * y));
+        double theta2 = theta - theta1 + Math.PI / 2;
 
         return new InverseKinematicsSolution(theta1, theta2);
     }

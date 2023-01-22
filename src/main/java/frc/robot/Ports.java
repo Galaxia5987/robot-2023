eeab5a133bfa0b228a08120d71d5dbb0a0d323bf
Alon Schwierz <@1>
2023-01-22 21:40:29 +0200

    Create ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
 public final class Ports {
-
+public class Intake{
+    public static final int INFERIOR_SPARK_MAX_PORT = 0;
+    public static final int SUPERIOR_SPARK_MAX_PORT = 0;
+    public static final int SOLENOID = 0;
+}
 }

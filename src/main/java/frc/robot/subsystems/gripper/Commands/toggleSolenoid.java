151db369a8af30897b594afc8abeb139fcdf8b0f
Alon Schwierz <@1>
2023-01-22 17:19:21 +0200

    Create toggle Solenoid Command

---
+++ src/main/java/frc/robot/subsystems/gripper/Commands/toggleSolenoid.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.gripper.Commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.gripper.Gripper;
+
+public class toggleSolenoid extends CommandBase {
+    private Gripper gripper;
+    @Override
+    public void initialize() {
+        gripper.toggleSolenoid();
+    }
+
+}

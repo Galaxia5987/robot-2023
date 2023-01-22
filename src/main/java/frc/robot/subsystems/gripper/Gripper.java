befe2d373488e25a8f0816fbc08af61ee3858005
Alon Schwierz <@1>
2023-01-22 20:27:24 +0200

    Documentation and change periodic to logInputs

--- src/main/java/frc/robot/subsystems/gripper/Gripper.java
+++ src/main/java/frc/robot/subsystems/gripper/Gripper.java
@@ -1,21 +1,41 @@
 package frc.robot.subsystems.gripper;
 
+import edu.wpi.first.networktables.BooleanSubscriber;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Ports;
+import frc.robot.subsystems.LoggedSubsystem;
 
-public class Gripper extends SubsystemBase {
+public class Gripper extends LoggedSubsystem<GripperLoggedInputs> {
+    private GripperLoggedInputs gripperLoggedInputs;
     private final Solenoid leftSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Gripper.LEFT_SOLENOID);
     private final Solenoid rightSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Gripper.RIGHT_SOLENOID);
 
-   private Gripper(){
-   }
+    private final NetworkTable table = NetworkTableInstance.getDefault().getTable("gripper");
+
+
+    public Gripper(GripperLoggedInputs inputs) {
+        super(inputs);
+    }
+
+    /**
+     * setSolenoid sets the Solenoid state
+     * @param state
+     */
+
     public void setSolenoid(boolean state) {
         leftSolenoid.set(state);
         rightSolenoid.set(state);
 
     }
+
+    /**
+     * changes the current state to the other state
+     */
 
     public void toggleSolenoid() {
         leftSolenoid.toggle();
@@ -23,9 +43,31 @@ public class Gripper extends SubsystemBase {
 
     }
 
-    public void getSolenoid() {
+    /**
+     * return the current state of the solenoids
+     */
+    public void getRightSolenoid() {
         rightSolenoid.get();
-        leftSolenoid.get();
+    }
 
+    public void getLeftSolenoid() {
+        leftSolenoid.get();
+    }
+
+    /**
+     * update the logger inputs
+     */
+
+    @Override
+    public void updateInputs() {
+        gripperLoggedInputs.leftSolenoid = leftSolenoid.get();
+
+        gripperLoggedInputs.rightSolenoid = rightSolenoid.get();
+
+    }
+
+    @Override
+    public String getSubsystemName() {
+        return "Gripper";
     }
 }

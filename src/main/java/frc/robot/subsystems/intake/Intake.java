411beee1e9f1d6f9b6f79aeb80b79a7fac7e7c57
Alon Schwierz <@1>
2023-01-25 22:59:04 +0200

    Create BeamBreaker functions

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -6,13 +6,15 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import frc.robot.Ports;
 import frc.robot.subsystems.LoggedSubsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
+
 
 public class Intake extends LoggedSubsystem<IntakeLoggedInputs> {
     public static Intake INSTANCE;
     private final CANSparkMax motor = new CANSparkMax(Ports.Intake.MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
     private final Solenoid solenoid = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Intake.SOLENOID);
+    private final DigitalInput beamBreaker = new DigitalInput(Ports.Intake.BEAMBREAKER);
 
-    //TODO: add beamBreaker
 
     private Intake() {
         super(new IntakeLoggedInputs());
@@ -71,6 +73,26 @@ public class Intake extends LoggedSubsystem<IntakeLoggedInputs> {
      */
     public boolean getSolenoidState() {
         return solenoid.get();
+    }
+
+    public boolean getCurrentBeam() {
+        return beamBreaker.get();
+    }
+
+    /**
+     * this function is supposed to return whether a cube has passed through the intake( I have no idea if that's correct or not so please don't publicly execute me :) )
+     */
+    public String hasPassed() throws InterruptedException {
+        if (getCurrentBeam() == false) {
+            wait(3000);
+            if (getCurrentBeam() == true) {
+                return "a cube has passed";
+            } else {
+                return "a cube hasn't passed";
+            }
+        } else {
+            return "a cube isn't passing";
+        }
     }
 
     /**

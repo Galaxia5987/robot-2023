ceed0241d4c470314f5c7693bc49169b866b20b5
Alon Schwierz <@1>
2023-01-25 22:58:36 +0200

    Create a port for BeamBreaker

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -4,5 +4,6 @@ public final class Ports {
 public class Intake{
     public static final int MOTOR = 0;
     public static final int SOLENOID = 0;
+    public static final int BEAMBREAKER = 0;
 }
 }

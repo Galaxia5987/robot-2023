082d77414b056cf195036fd70e531046b97134da
Alon Schwierz <@1>
2023-01-29 20:19:31 +0200

    add the additional motor

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,8 +3,7 @@ package frc.robot;
 public final class Ports {
 public class Intake{
     public static final int MOTOR = 0;
-    public static final int LEFT_SOLENOID = 0;
-    public static final int RIGHT_SOLENOID = 0;
-    public static final int BEAMBREAKER = 0;
+    public static final int ANGLE_MOTOR = 0;
+    public static final int BEAM_BREAKER_SENSOR = 0;
 }
 }

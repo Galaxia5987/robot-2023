e52994e3b1e3379ce546a16d490450c30f5d6f11
Alon Schwierz <@1>
2023-01-29 20:18:19 +0200

    Change name for constants

---
+++ src/main/java/frc/robot/subsystems/intake/ConstantsIntake.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.intake;
+
+public class Constants {
+    public static final double GEAR_RATIO  = 1/26.07;
+    public static final double TICKS_PER_RADIAN  = 42*GEAR_RATIO/2*Math.PI;
+
+}

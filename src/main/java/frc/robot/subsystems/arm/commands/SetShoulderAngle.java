bafb17ea14d8edd7fda968abbf9326cb4ebd3ce0
Emma <emmalivne.03@gmail.com>
2023-01-29 22:41:13 +0200

    Add deadband

--- src/main/java/frc/robot/subsystems/arm/commands/SetShoulderAngle.java
+++ src/main/java/frc/robot/subsystems/arm/commands/SetShoulderAngle.java
@@ -1,14 +1,16 @@
 package frc.robot.subsystems.arm.commands;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.ArmConstants;
 
 public class SetShoulderAngle extends CommandBase {
     private final Arm arm;
     private final double angle;
 
-    public SetShoulderAngle(Arm arm, double angle) {
-        this.arm = arm;
+    public SetShoulderAngle(double angle) {
+        this.arm = Arm.getInstance();
         this.angle = angle;
         addRequirements(arm);
     }
@@ -20,9 +22,11 @@ public class SetShoulderAngle extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        if (arm.getShoulderJointAngle() == angle) {
-            return true;
-        }
-        return false;
+        return MathUtil.applyDeadband(angle, ArmConstants.SETPOINT_DEADBAND) == arm.getShoulderJointAngle();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        arm.stop();
     }
 }

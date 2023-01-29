76d8f32e96f3b536a2d852bc4b7e1dc7fe85ffd2
Alon Schwierz <@1>
2023-01-29 20:23:01 +0200

    Create P, I and D constants.

--- src/main/java/frc/robot/subsystems/intake/ConstantsIntake.java
+++ src/main/java/frc/robot/subsystems/intake/ConstantsIntake.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems.intake;
 
-public class Constants {
+public class ConstantsIntake {
     public static final double GEAR_RATIO  = 1/26.07;
     public static final double TICKS_PER_RADIAN  = 42*GEAR_RATIO/2*Math.PI;
-
+public static final double kP = 0;
+public static final double kI = 0;
+public static final double kD = 0;
 }

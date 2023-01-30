5ebb629c0ff1d703693cc82c4942d876369ff959
Alon Schwierz <@1>
2023-01-30 18:18:15 +0200

    Make the sensor object private and final

--- src/main/java/frc/robot/subsystems/intake/BeamBreaker.java
+++ src/main/java/frc/robot/subsystems/intake/BeamBreaker.java
@@ -6,7 +6,7 @@ import frc.robot.subsystems.LoggedSubsystem;
 
 
 public class BeamBreaker extends LoggedSubsystem<BeamBreakerLoggedInputs> {
-    public DigitalInput beam = new DigitalInput(Ports.Intake.BEAM_BREAKER_SENSOR);
+    private final DigitalInput beam = new DigitalInput(Ports.Intake.BEAM_BREAKER_SENSOR);
     boolean isBlocking;
 
     public BeamBreaker(BeamBreakerLoggedInputs inputs) {

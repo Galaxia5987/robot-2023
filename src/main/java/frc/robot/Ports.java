4cf3de51af144168641355a075f5213a041d7d79
GaiaZ <gaya.zano11@gmail.com>
2023-01-31 20:31:55 +0200

    Make class static

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -34,10 +34,12 @@ public final class Ports {
         public static boolean REAR_RIGHT_ANGLE_INVERTED = true;
         public static boolean REAR_RIGHT_ANGLE_SENSOR_PHASE = false;
     }
-    public class Gripper {
+
+    public static class Gripper {
         public static final int SOLENOID = 0;
     }
-    public class Intake {
+
+    public static class Intake {
         public static final int MOTOR = 0;
         public static final int ANGLE_MOTOR = 0;
         public static final int BEAM_BREAKER_SENSOR = 0;

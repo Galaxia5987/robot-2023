b17fafe8cdb91f762d7363ef1a460566e7dc495b
Emma <emmalivne.03@gmail.com>
2023-01-31 22:14:49 +0200

    remove TODO

--- src/main/java/frc/robot/commandGroups/TapeCommandGroup.java
+++ src/main/java/frc/robot/commandGroups/TapeCommandGroup.java
@@ -1,13 +1,17 @@
 package frc.robot.commandGroups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.autonomous.FollowPath;
 import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.arm.commands.SetArmsPosition;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
+import frc.robot.subsystems.gyroscope.Gyroscope;
+import frc.robot.subsystems.vision.Limelight;
 
 public class TapeCommandGroup extends SequentialCommandGroup {
     public TapeCommandGroup() {
         addCommands(
-                //TODO: add following path to reflective tape target - thank you Eitan!
+                FollowPath.generatePathToAprilTag(new SwerveDrive(), Limelight.getInstance(), new Gyroscope())
         );
     }
 }

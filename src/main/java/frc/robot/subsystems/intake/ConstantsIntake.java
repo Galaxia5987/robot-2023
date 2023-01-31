2fa6830f707efef8b5d0e0ad8bce84b9790f5a72
Alon Schwierz <@1>
2023-01-31 21:04:55 +0200

    create position constants for Intake

--- src/main/java/frc/robot/subsystems/intake/ConstantsIntake.java
+++ src/main/java/frc/robot/subsystems/intake/ConstantsIntake.java
@@ -3,7 +3,8 @@ package frc.robot.subsystems.intake;
 public class ConstantsIntake {
     public static final double GEAR_RATIO = 1 / 26.07;
     public static final double TICKS_PER_DEGREE = (42 * GEAR_RATIO) / 360;
-//TODO: check whether the calculation is correct.
+    public static final double INTAKE_CLOSED_POSITION = 0;
+    public static final double INTAKE_OPENED_POSITION = 0;
     public static final double kP = 0;
     public static final double kI = 0;
     public static final double kD = 0;

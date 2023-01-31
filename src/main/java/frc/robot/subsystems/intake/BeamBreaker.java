4128ca6cad34db54bdda135c7a41fdac4715c836
Alon Schwierz <@1>
2023-01-31 21:11:05 +0200

    Create Instance and get method for BeamBreaker

--- src/main/java/frc/robot/subsystems/intake/BeamBreaker.java
+++ src/main/java/frc/robot/subsystems/intake/BeamBreaker.java
@@ -7,9 +7,17 @@ import frc.robot.subsystems.LoggedSubsystem;
 
 public class BeamBreaker extends LoggedSubsystem<BeamBreakerLoggedInputs> {
     private final DigitalInput beam = new DigitalInput(Ports.Intake.BEAM_BREAKER_SENSOR);
+    private static BeamBreaker INSTANCE;
 
     public BeamBreaker(BeamBreakerLoggedInputs inputs) {
         super(inputs);
+    }
+
+    public static BeamBreaker getInstance() {
+        if(INSTANCE == null){
+            INSTANCE = new BeamBreaker(new BeamBreakerLoggedInputs());
+        }
+        return INSTANCE;
     }
 
     /**

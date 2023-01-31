3c63937e3ac72384108cbb51abe42602d7a7228d
GaiaZ <gaya.zano11@gmail.com>
2023-01-31 20:32:06 +0200

    Add run all bits

---
+++ src/main/java/frc/robot/commandgroups/bits/RunAllBits.java
@@ -0,0 +1,20 @@
+package frc.robot.commandgroups.bits;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Ports;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
+import frc.robot.subsystems.gripper.Gripper;
+import frc.robot.subsystems.intake.BeamBreaker;
+import frc.robot.subsystems.intake.Intake;
+
+public class RunAllBits extends SequentialCommandGroup {
+    public RunAllBits(SwerveDrive swerveDrive, Intake intake, Gripper gripper, Arm arm, BeamBreaker beamBreaker) {
+        addCommands(
+                new CheckIntakeFlow(intake, beamBreaker, gripper),
+                new CheckArmPositions(arm),
+                new ZeroPositionSwerve(swerveDrive),
+                new CheckSwerve(swerveDrive)
+        );
+    }
+}

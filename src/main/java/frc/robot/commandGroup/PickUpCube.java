9fadc0c26e6772219ca6db2d7edc5ec91fa914a1
Alon Schwierz <@1>
2023-01-31 21:05:13 +0200

    Change parameters

--- src/main/java/frc/robot/commandGroup/PickUpCube.java
+++ src/main/java/frc/robot/commandGroup/PickUpCube.java
@@ -5,13 +5,15 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.arm.commands.SetArmsPosition;
 import frc.robot.subsystems.gripper.Gripper;
+import frc.robot.subsystems.intake.BeamBreaker;
 import frc.robot.subsystems.intake.Command.Feed;
+import frc.robot.subsystems.intake.Intake;
 
 public class PickUpCube extends SequentialCommandGroup {
-    public PickUpCube(Gripper gripper, double intakePower, Translation2d armPosition) {
+    public PickUpCube(Gripper gripper, double intakePower, Translation2d armPosition, Intake intake, BeamBreaker beamBreaker) {
         addCommands(
                 // Feeds the cube into the Intake until the BeamBreaker sends a false signal
-                new Feed(intakePower, 0),
+                new Feed(intakePower, intake, beamBreaker),
                 //Sets the arm position into the inside space of the Robot and meanwhile opens the gripper
                 new SetArmsPosition(armPosition).alongWith(new InstantCommand(()-> gripper.open())),
                 //Closes the Gripper after it is set on the position of the cube.

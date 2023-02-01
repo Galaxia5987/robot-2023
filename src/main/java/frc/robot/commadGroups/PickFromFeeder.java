fd6ec188edffff9c2c78d2212368d3a46b506619
Alon Schwierz <@1>
2023-02-01 20:57:50 +0200

    Change constructor parameters and fields for the commandGroup

--- src/main/java/frc/robot/commadGroups/PickFromFeeder.java
+++ src/main/java/frc/robot/commadGroups/PickFromFeeder.java
@@ -3,16 +3,14 @@ package frc.robot.commadGroups;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.commands.SetArmsPosition;
 import frc.robot.subsystems.gripper.Gripper;
 
-import java.lang.management.GarbageCollectorMXBean;
 
 public class PickFromFeeder extends SequentialCommandGroup {
-    public PickFromFeeder(Arm arm, Translation2d armStartPosition, Translation2d armEndPosition, Gripper gripper){
+    private Gripper gripper = Gripper.getInstance();
+    public PickFromFeeder(Translation2d armStartPosition, Translation2d armEndPosition){
         addCommands(
-                //TODO: use the drivetrain commands in order to set the robot to the right angle and position
                 new SetArmsPosition(armStartPosition).alongWith(new InstantCommand(()-> gripper.open())),
                 new InstantCommand(()-> gripper.close()),
                 new SetArmsPosition(armEndPosition)

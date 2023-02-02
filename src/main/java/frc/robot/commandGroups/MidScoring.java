bc3a7593cd68269d3b0d40eb889f2029be043011
Emma <emmalivne.03@gmail.com>
2023-02-02 11:58:06 +0200

    remove unnecessary classes

--- src/main/java/frc/robot/commandGroups/MidScoring.java
+++ src/main/java/frc/robot/commandGroups/MidScoring.java
@@ -3,9 +3,12 @@ package frc.robot.commandGroups;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.autonomous.FollowPath;
 import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.arm.commands.SetArmsPosition;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.gripper.Gripper;
+import frc.robot.subsystems.gyroscope.Gyroscope;
 import frc.robot.subsystems.vision.Limelight;
 import frc.robot.subsystems.vision.VisionConstants;
 
@@ -15,8 +18,8 @@ public class MidScoring extends SequentialCommandGroup {
     public MidScoring(){
         addCommands(
                 new ConditionalCommand(
-                        new TapeCommandGroup(),
-                        new AprilTagCommandGroup(),
+                        FollowPath.generatePathToAprilTag(new SwerveDrive(), limelight, new Gyroscope()), //TODO: ask Eitan how to use this with reflective tape
+                        FollowPath.generatePathToAprilTag(new SwerveDrive(), limelight, new Gyroscope()),
                         () -> limelight.getPipeline() == VisionConstants.REFLECTIVE_TAPE_PIPELINE
                 ),
                 new SetArmsPosition(() -> limelight.getPipeline() == VisionConstants.REFLECTIVE_TAPE_PIPELINE ? ArmConstants.MIDDLE_CONE_SCORING : ArmConstants.MIDDLE_CUBE_SCORING),
--- src/main/java/frc/robot/commandGroups/UpperScoring.java
+++ src/main/java/frc/robot/commandGroups/UpperScoring.java
@@ -3,9 +3,12 @@ package frc.robot.commandGroups;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.autonomous.FollowPath;
 import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.arm.commands.SetArmsPosition;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.gripper.Gripper;
+import frc.robot.subsystems.gyroscope.Gyroscope;
 import frc.robot.subsystems.vision.Limelight;
 import frc.robot.subsystems.vision.VisionConstants;
 
@@ -16,8 +19,8 @@ public class UpperScoring extends SequentialCommandGroup {
     public UpperScoring() {
         addCommands(
                 new ConditionalCommand(
-                        new TapeCommandGroup(),
-                        new AprilTagCommandGroup(),
+                        FollowPath.generatePathToAprilTag(new SwerveDrive(), limelight, new Gyroscope()), //TODO: ask Eitan how to use this with reflective tape
+                        FollowPath.generatePathToAprilTag(new SwerveDrive(), limelight, new Gyroscope()),
                         () -> limelight.getPipeline() == VisionConstants.REFLECTIVE_TAPE_PIPELINE
                 ),
                 new SetArmsPosition(() -> limelight.getPipeline() == VisionConstants.REFLECTIVE_TAPE_PIPELINE ? ArmConstants.UPPER_CONE_SCORING : ArmConstants.UPPER_CUBE_SCORING),

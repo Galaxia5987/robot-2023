5e30cca21048a82bd2e4404585bf60e80ecd5b3a
GaiaZ <gaya.zano11@gmail.com>
2023-02-02 20:53:56 +0200

    BC

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 303;
-  public static final String GIT_SHA = "2ba636fcf12827fa036fa0ff49aee6a80aaf7fe2";
-  public static final String GIT_DATE = "2023-02-02 11:42:43 EST";
-  public static final String GIT_BRANCH = "feature/organize";
-  public static final String BUILD_DATE = "2023-02-02 13:36:27 EST";
-  public static final long BUILD_UNIX_TIME = 1675362987255L;
+  public static final int GIT_REVISION = 312;
+  public static final String GIT_SHA = "1a05657fbc24598def1eec159a06501ba18bd6a8";
+  public static final String GIT_DATE = "2023-02-02 13:42:37 EST";
+  public static final String GIT_BRANCH = "integration";
+  public static final String BUILD_DATE = "2023-02-02 13:53:13 EST";
+  public static final long BUILD_UNIX_TIME = 1675363993552L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

772cd0f02974b1520612f33e11a91b46fedd1b56
Emma <emmalivne.03@gmail.com>
2023-02-03 13:22:40 +0200

    Add SwerveDrive instance

--- src/main/java/frc/robot/autonomous/FollowPath.java
+++ src/main/java/frc/robot/autonomous/FollowPath.java
@@ -303,7 +303,8 @@ public class FollowPath extends CommandBase {
         });
     }
 
-    public static FollowPath loadTrajectory(String path, SwerveDrive swerveDrive){
+    public static FollowPath loadTrajectory(String path){
+        SwerveDrive swerveDrive = SwerveDrive.getInstance();
         return new FollowPath(
                 PathPlanner.loadPath(path, SwerveConstants.MAX_VELOCITY_METERS_PER_SECOND, SwerveConstants.MAX_LINEAR_ACCELERATION),
                 swerveDrive::getPose,

ff142106433779d78617ee1f0129fc2aab021e29
GaiaZ <gaya.zano11@gmail.com>
2023-02-04 18:22:27 +0200

    Add  pipeline command

--- src/main/java/frc/robot/commandgroups/TakeConeFromFeeder.java
+++ src/main/java/frc/robot/commandgroups/TakeConeFromFeeder.java
@@ -1,20 +1,19 @@
-package frc.robot.command_groups;
+package frc.robot.commandgroups;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.autonomous.FollowPath;
 import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.arm.commands.SetArmsPosition;
-import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.gripper.Gripper;
-import frc.robot.subsystems.gyroscope.Gyroscope;
 import frc.robot.subsystems.vision.Limelight;
 
 public class TakeConeFromFeeder extends SequentialCommandGroup {
 
     public TakeConeFromFeeder(boolean rightSide) {
         Gripper gripper = Gripper.getInstance();
+        Limelight limelight = Limelight.getInstance();
         addCommands(
+                new InstantCommand(limelight::setAprilTagsPipeline, limelight),
                 new AdjustToAprilTag(rightSide, true)
                         .alongWith(new InstantCommand(gripper::open)),
                 new SetArmsPosition(ArmConstants.FEEDER_POSITION),

b6ccb0d9fe05747e8e5699196132c1e8870ccc7a
GaiaZano05 <gaya.zano11@gmail.com>
2023-02-07 17:13:04 +0200

    Add joystick buttons

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,10 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ProxyCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.autonomous.FollowPath;
+import frc.robot.commandgroups.AdjustToAprilTag;
+import frc.robot.commandgroups.AdjustToTarget;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.drivetrain.SwerveConstants;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
@@ -33,9 +36,12 @@ public class RobotContainer {
     private final JoystickButton b = new JoystickButton(xboxController, XboxController.Button.kB.value);
     private final JoystickButton rb = new JoystickButton(xboxController, XboxController.Button.kRightBumper.value);
     private final JoystickButton lb = new JoystickButton(xboxController, XboxController.Button.kLeftBumper.value);
-    private final JoystickButton leftTrigger = new JoystickButton(leftJoystick, 1);
-    private final JoystickButton rightTrigger = new JoystickButton(rightJoystick, 1);
-    private Command teleopTargetAdjustCommand;
+    private final JoystickButton leftJoystickTrigger = new JoystickButton(leftJoystick, Ports.UI.JOYSTICK_TRIGGER);
+    private final JoystickButton leftJoystickTopBottom = new JoystickButton(leftJoystick, Ports.UI.JOYSTICK_TOP_BOTTOM_BUTTON);
+    private final JoystickButton rightJoystickTrigger = new JoystickButton(rightJoystick, Ports.UI.JOYSTICK_TRIGGER);
+    private final JoystickButton rightJoystickTopBottom = new JoystickButton(rightJoystick, Ports.UI.JOYSTICK_TOP_BOTTOM_BUTTON);
+    private final Trigger leftPOV = new Trigger(() -> xboxController.getPOV() == 270);
+    private final Trigger rightPOV = new Trigger(() -> xboxController.getPOV() == 90);
 
     /**
      * The container for the robot.  Contains subsystems, OI devices, and commands.
@@ -54,18 +60,18 @@ public class RobotContainer {
     }
 
     private void configureDefaultCommands() {
-//        swerveSubsystem.setDefaultCommand(
-//                new JoystickDrive(swerveSubsystem, leftJoystick, rightJoystick)
-//        );
         swerveSubsystem.setDefaultCommand(
-                new XboxDrive(swerveSubsystem, xboxController)
+                new JoystickDrive(swerveSubsystem, leftJoystick, rightJoystick)
         );
     }
 
     private void configureButtonBindings() {
-        a.whileTrue(new ProxyCommand(() -> teleopTargetAdjustCommand));
-        rb.onTrue(new InstantCommand(gyroscope::resetYaw));
-        b.whileTrue(new BalanceOnStation());
+        rightJoystickTrigger.onTrue(new InstantCommand(gyroscope::resetYaw));
+        leftJoystickTrigger.whileTrue(new AdjustToTarget(false, false));
+        rightJoystickTopBottom.whileTrue(new BalanceOnStation());
+
+        leftPOV.whileTrue(new AdjustToTarget(false, true));
+        rightPOV.whileTrue(new AdjustToTarget(true, true));
     }
 
 

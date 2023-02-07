189f7c22a97bb1c818867f5960e302829feac1b7
GaiaZano05 <gaya.zano11@gmail.com>
2023-02-07 17:38:22 +0200

    Add swerve and arm bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,32 +10,40 @@ import edu.wpi.first.wpilibj2.command.ProxyCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.autonomous.FollowPath;
-import frc.robot.commandgroups.AdjustToAprilTag;
-import frc.robot.commandgroups.AdjustToTarget;
+import frc.robot.commandgroups.*;
 import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.ArmConstants;
+import frc.robot.subsystems.arm.commands.ArmJoystickControl;
+import frc.robot.subsystems.arm.commands.ArmXboxControl;
 import frc.robot.subsystems.drivetrain.SwerveConstants;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.drivetrain.commands.BalanceOnStation;
 import frc.robot.subsystems.drivetrain.commands.JoystickDrive;
 import frc.robot.subsystems.drivetrain.commands.XboxDrive;
 import frc.robot.subsystems.gyroscope.Gyroscope;
+import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.leds.Leds;
 import frc.robot.subsystems.vision.Limelight;
 
 public class RobotContainer {
-//    private static final Leds led = Leds.getInstance();
-//    private static final Arm arm = Arm.getInstance();
-    public static Gyroscope gyroscope = new Gyroscope();
-    public static SwerveDrive swerveSubsystem = new SwerveDrive();
     private static RobotContainer INSTANCE = null;
-    private final XboxController xboxController = new XboxController(0);
+    private final Leds led = Leds.getInstance();
+    private final Arm arm = Arm.getInstance();
+    private final Gyroscope gyroscope = new Gyroscope();
+    private final SwerveDrive swerveSubsystem = new SwerveDrive();
     private final Limelight limelight = Limelight.getInstance();
+    private final Intake intake = Intake.getInstance();
+    private final XboxController xboxController = new XboxController(0);
     private final Joystick leftJoystick = new Joystick(1);
     private final Joystick rightJoystick = new Joystick(2);
     private final JoystickButton a = new JoystickButton(xboxController, XboxController.Button.kA.value);
     private final JoystickButton b = new JoystickButton(xboxController, XboxController.Button.kB.value);
+    private final JoystickButton y = new JoystickButton(xboxController, XboxController.Button.kY.value);
+    private final JoystickButton x = new JoystickButton(xboxController, XboxController.Button.kX.value);
     private final JoystickButton rb = new JoystickButton(xboxController, XboxController.Button.kRightBumper.value);
     private final JoystickButton lb = new JoystickButton(xboxController, XboxController.Button.kLeftBumper.value);
+    private final Trigger xboxRightTrigger = new Trigger(() -> xboxController.getRightTriggerAxis() > 0.2);
+    private final Trigger xboxLeftTrigger = new Trigger(() -> xboxController.getLeftTriggerAxis() > 0.2);
     private final JoystickButton leftJoystickTrigger = new JoystickButton(leftJoystick, Ports.UI.JOYSTICK_TRIGGER);
     private final JoystickButton leftJoystickTopBottom = new JoystickButton(leftJoystick, Ports.UI.JOYSTICK_TOP_BOTTOM_BUTTON);
     private final JoystickButton rightJoystickTrigger = new JoystickButton(rightJoystick, Ports.UI.JOYSTICK_TRIGGER);
@@ -63,6 +71,7 @@ public class RobotContainer {
         swerveSubsystem.setDefaultCommand(
                 new JoystickDrive(swerveSubsystem, leftJoystick, rightJoystick)
         );
+        arm.setDefaultCommand(new ArmXboxControl(xboxController));
     }
 
     private void configureButtonBindings() {
@@ -72,6 +81,11 @@ public class RobotContainer {
 
         leftPOV.whileTrue(new AdjustToTarget(false, true));
         rightPOV.whileTrue(new AdjustToTarget(true, true));
+
+        a.onTrue(new FloorScoring());
+        b.onTrue(new MidScoring());
+        y.onTrue(new UpperScoring());
+        x.onTrue(new PickFromFeeder(ArmConstants.FEEDER_POSITION, ArmConstants.FEEDER_POSITION, true));
     }
 
 

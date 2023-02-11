d23452c7c00089bb8178987a51ef59897dfdc9ef
eitan <eitan.vichik@gmail.com>
2023-02-11 12:01:16 +0200

    Revert RevLib update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 381;
-  public static final String GIT_SHA = "d9f2eeb043fe2c5052dca4779f3df6d25e34a61b";
-  public static final String GIT_DATE = "2023-02-04 11:57:21 EST";
+  public static final int GIT_REVISION = 382;
+  public static final String GIT_SHA = "1a6aef7e1f0dc2cb858ec7b182d79939324cb2b0";
+  public static final String GIT_DATE = "2023-02-11 04:58:22 EST";
   public static final String GIT_BRANCH = "integration";
-  public static final String BUILD_DATE = "2023-02-11 04:56:10 EST";
-  public static final long BUILD_UNIX_TIME = 1676109370184L;
+  public static final String BUILD_DATE = "2023-02-11 05:01:08 EST";
+  public static final long BUILD_UNIX_TIME = 1676109668337L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

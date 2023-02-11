f87ce589d044e8b2046efd75e900f2143bea73b9
eitan <eitan.vichik@gmail.com>
2023-02-11 09:07:07 +0200

    Fix compilation errors

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 364;
-  public static final String GIT_SHA = "d3a689f423a5d8345863d968396024ad268ae8ba";
-  public static final String GIT_DATE = "2023-02-04 11:13:20 EST";
-  public static final String GIT_BRANCH = "feature/paths";
-  public static final String BUILD_DATE = "2023-02-04 11:19:31 EST";
-  public static final long BUILD_UNIX_TIME = 1675527571765L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 385;
+  public static final String GIT_SHA = "d006ad72aa0acf1bf4904f41d8ac76042cf14854";
+  public static final String GIT_DATE = "2023-02-07 10:42:55 EST";
+  public static final String GIT_BRANCH = "feature/button-bindings";
+  public static final String BUILD_DATE = "2023-02-11 02:06:11 EST";
+  public static final long BUILD_UNIX_TIME = 1676099171996L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }

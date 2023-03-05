8cec348c7a0e25e5db1b15242202a3fb8d893b82
GaiaZano05 <gaya.zano11@gmail.com>
2023-03-05 14:16:52 +0200

    Bits changes

--- src/main/java/frc/robot/commandgroups/bits/CheckArmPositions.java
+++ src/main/java/frc/robot/commandgroups/bits/CheckArmPositions.java
@@ -1,19 +1,15 @@
 package frc.robot.commandgroups.bits;
 
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commandgroups.GetArmIntoRobot;
 import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.arm.commands.SetArmsPositionAngular;
 
 public class CheckArmPositions extends SequentialCommandGroup {
     public CheckArmPositions() {
-        new SetArmsPositionAngular(() -> new Translation2d(0, 0), 0.02);
+        new GetArmIntoRobot().withTimeout(3);
         new WaitCommand(1);
-        new SetArmsPositionAngular(() -> new Translation2d(0, 0), 0.02);
-        new WaitCommand(1);
-        new SetArmsPositionAngular(() -> new Translation2d(0, 0), 0.02);
-        new WaitCommand(1);
-        new SetArmsPositionAngular(() -> ArmConstants.ABOVE_GAME_PIECE, 0.02);
+        new SetArmsPositionAngular(() -> ArmConstants.STARTING_POSITION, 0.02).withTimeout(3);
     }
 }

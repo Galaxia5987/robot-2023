3e7f5df6a506bcc0f11fdbe7b874aa49dfb031ad
GaiaZano05 <gaya.zano11@gmail.com>
2023-03-07 20:14:40 +0200

    Create Command

--- src/main/java/frc/robot/subsystems/drivetrain/commands/AdjustToTargetSmart.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/AdjustToTargetSmart.java
@@ -1,83 +1,32 @@
-//package frc.robot.subsystems.drivetrain.commands;
-//
-//import edu.wpi.first.math.geometry.Pose2d;
-//import edu.wpi.first.math.geometry.Rotation2d;
-//import edu.wpi.first.math.geometry.Transform2d;
-//import edu.wpi.first.math.geometry.Translation2d;
-//import edu.wpi.first.math.kinematics.ChassisSpeeds;
-//import edu.wpi.first.wpilibj2.command.CommandBase;
-//import frc.robot.subsystems.drivetrain.DriveSignal;
-//import frc.robot.subsystems.drivetrain.SwerveConstants;
-//import frc.robot.subsystems.drivetrain.SwerveDrive;
-//import frc.robot.subsystems.gyroscope.Gyroscope;
-//import frc.robot.subsystems.vision.Limelight;
-//import frc.robot.utils.controllers.PIDFController;
-//import org.littletonrobotics.junction.Logger;
-//
-//public class AdjustToTargetSmart extends CommandBase {
-//    private final SwerveDrive swerveDrive = SwerveDrive.getInstance();
-//    private final Limelight limelight = Limelight.getInstance();
-//    private final Gyroscope gyroscope = Gyroscope.getInstance();
-//
-//    private Pose2d finalPose;
-//    private final Position position;
-//
-//    private final PIDFController xController = new PIDFController(SwerveConstants.TARGET_XY_Kp, SwerveConstants.TARGET_XY_Ki, SwerveConstants.TARGET_XY_Kd, SwerveConstants.TARGET_XY_Kf);
-//    private final PIDFController yController = new PIDFController(SwerveConstants.TARGET_XY_Kp, SwerveConstants.TARGET_XY_Ki, SwerveConstants.TARGET_XY_Kd, SwerveConstants.TARGET_XY_Kf);
-//    private final PIDFController rotationController = new PIDFController(SwerveConstants.TARGET_ROTATION_Kp, SwerveConstants.TARGET_ROTATION_Ki, SwerveConstants.TARGET_ROTATION_Kd, SwerveConstants.TARGET_ROTATION_Kf);
-//
-//    public AdjustToTargetSmart(Position position) {
-//        this.position = position;
-//        addRequirements(swerveDrive);
-//    }
-//
-//    @Override
-//    public void initialize() {
-//        var botPose = limelight.getBotPose();
-//        Pose2d startPose = swerveDrive.getPose();
-//
-//        botPose.ifPresent(pose2d -> finalPose = startPose.plus(new Transform2d(pose2d.getTranslation()
-//                .minus(position.offset), new Rotation2d())));
-//        if(botPose.isPresent()){
-//            pose2d -> finalPose = startPose.plus(new Transform2d(pose2d.getTranslation()
-//                    .minus(position.offset), new Rotation2d())
-//        }
-//
-//        Logger.getInstance().recordOutput("finalPose", finalPose.toString());
-//        Logger.getInstance().recordOutput("startPose", startPose.toString());
-//    }
-//
-//    @Override
-//    public void execute() {
-//        var pose = swerveDrive.getPose();
-//        var speeds = new ChassisSpeeds(
-//                xController.calculate(pose.getX(), finalPose.getX()),
-//                yController.calculate(pose.getY(), finalPose.getY()),
-//                rotationController.calculate(gyroscope.getYaw().getRadians(), 0)
-//        );
-//
-//        swerveDrive.drive(
-//                new DriveSignal(
-//                        speeds,
-//                        new Translation2d(),
-//                        true
-//                )
-//        );
-//    }
-//
-//    public enum Position {
-//        RIGHT(new Translation2d(-0.75, 0.69)),
-//        LEFT(new Translation2d(-0.75, -0.51)),
-//        MIDDLE(new Translation2d(-0.75, 0.13));
-//
-//        public final Translation2d offset;
-//
-//        Position(Translation2d offset) {
-//            this.offset = offset;
-//        }
-//
-//        public Pose2d getFinalPose(Pose2d currentPose) {
-//            return new Pose2d(currentPose.getTranslation().minus(this.offset), new Rotation2d());
-//        }
-//    }
-//}
+package frc.robot.subsystems.drivetrain.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.drivetrain.SwerveConstants;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
+import frc.robot.subsystems.gyroscope.Gyroscope;
+import frc.robot.subsystems.vision.Limelight;
+import frc.robot.utils.controllers.PIDFController;
+
+public class AdjustToTargetSmart extends CommandBase {
+    private final SwerveDrive swerveDrive = SwerveDrive.getInstance();
+    private final Limelight limelight = Limelight.getInstance();
+    private final Gyroscope gyroscope = Gyroscope.getInstance();
+
+
+    private final PIDFController xController = new PIDFController(SwerveConstants.TARGET_XY_Kp, SwerveConstants.TARGET_XY_Ki, SwerveConstants.TARGET_XY_Kd, SwerveConstants.TARGET_XY_Kf);
+    private final PIDFController yController = new PIDFController(SwerveConstants.TARGET_XY_Kp, SwerveConstants.TARGET_XY_Ki, SwerveConstants.TARGET_XY_Kd, SwerveConstants.TARGET_XY_Kf);
+    private final PIDFController rotationController = new PIDFController(SwerveConstants.TARGET_ROTATION_Kp, SwerveConstants.TARGET_ROTATION_Ki, SwerveConstants.TARGET_ROTATION_Kd, SwerveConstants.TARGET_ROTATION_Kf);
+
+    public AdjustToTargetSmart() {
+        addRequirements(swerveDrive);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+    }
+}

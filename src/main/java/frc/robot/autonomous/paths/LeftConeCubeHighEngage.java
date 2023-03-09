b297cd1f44ff9dbeb63bc450744b2e3ae615f3ca
Alon Schwierz <@1>
2023-03-09 18:17:15 +0200

    Add Engage in the end (*dramatic voices*)

---
+++ src/main/java/frc/robot/autonomous/paths/LeftConeCubeHighEngage.java
@@ -0,0 +1,67 @@
+package frc.robot.autonomous.paths;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.autonomous.FollowPath;
+import frc.robot.commandgroups.PickUpCube;
+import frc.robot.commandgroups.ReturnIntake;
+import frc.robot.subsystems.drivetrain.DriveSignal;
+import frc.robot.subsystems.drivetrain.SwerveConstants;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
+import frc.robot.subsystems.drivetrain.commands.DriveTillPitch;
+import frc.robot.subsystems.gripper.Gripper;
+import frc.robot.subsystems.gyroscope.Gyroscope;
+import frc.robot.subsystems.leds.PurpleLed;
+import frc.robot.utils.AllianceFlipUtil;
+
+/**
+ * This class contains all parts of the path LeftConeCubeHigh.
+ * <p>
+ * In this path the robot places a cone in the grid that is closest to the feeder,
+ * goes to take a cube (the one closest to the feeder) and returns to place it.
+ */
+public class LeftConeCubeHighEngage extends SequentialCommandGroup {
+    public LeftConeCubeHighEngage() {
+        Gyroscope gyroscope = Gyroscope.getInstance();
+        SwerveDrive swerveDrive = SwerveDrive.getInstance();
+        Gripper gripper = Gripper.getInstance();
+        PathPlannerTrajectory trajectory = PathPlanner.loadPath("LeftConeCubeHigh blue 1", new PathConstraints(SwerveConstants.MAX_VELOCITY_AUTO, SwerveConstants.MAX_ACCELERATION_AUTO));
+
+        addCommands(
+                new InstantCommand(() -> swerveDrive.resetOdometry(
+                        AllianceFlipUtil.apply(DriverStation.getAlliance(), trajectory.getInitialPose()))),
+                new InstantCommand(() -> gyroscope.resetYaw(trajectory.getInitialHolonomicPose().getRotation())),
+                new InstantCommand(() -> swerveDrive.resetOdometry(trajectory.getInitialPose())),
+
+
+
+                new PurpleLed(),
+
+                FollowPath.loadTrajectory("LeftConeCubeHigh blue 1").alongWith(
+                        new PickUpCube().withTimeout(4)),
+
+                FollowPath.loadTrajectory("LeftConeCubeHigh blue 2")
+                        .alongWith(new ReturnIntake()
+                                        .andThen(new InstantCommand(gripper::close, gripper)),
+                                FollowPath.loadTrajectory("LeftConeCubeHigh blue charge"),
+                                new RunCommand(() -> swerveDrive.drive(
+                                        new DriveSignal(
+                                                -1,
+                                                0,
+                                                0,
+                                                new Translation2d(),
+                                                true
+                                        )
+                                ), swerveDrive).withTimeout(1.35),
+
+                                new RunCommand(swerveDrive::lock)
+
+                        ));
+    }
+}

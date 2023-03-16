2563e16c0eb05a2799fd43a3b1a5a5b94e4ddb1f
GaiaZano05 <gaya.zano11@gmail.com>
2023-03-16 12:34:29 +0200

    Fix offset in UpperScoring arm position

--- src/main/java/frc/robot/commandgroups/UpperScoring.java
+++ src/main/java/frc/robot/commandgroups/UpperScoring.java
@@ -1,6 +1,7 @@
 package frc.robot.commandgroups;
 
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.ArmConstants;
@@ -10,12 +11,14 @@ import frc.robot.subsystems.leds.Leds;
 public class UpperScoring extends SequentialCommandGroup {
 
     public UpperScoring() {
+        Arm arm = Arm.getInstance();
         addCommands(
                 new ConditionalCommand(
                         new SetArmsPositionAngular(() -> ArmConstants.UPPER_CONE_SCORING, 0.1),
                         new SetArmsPositionAngular(() -> ArmConstants.UPPER_CUBE_SCORING),
                         Leds.getInstance()::inConeMode
-                )
+                ),
+                new RunCommand(() -> arm.setShoulderJointPower(-0.05), arm).withTimeout(0.5)
         );
     }
 }

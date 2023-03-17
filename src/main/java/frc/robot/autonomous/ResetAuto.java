2d73e7bccb057f52d16fd54e8757c5dd98795b16
GaiaZano05 <gaya.zano11@gmail.com>
2023-03-17 15:51:27 +0200

    popo dick salat

--- src/main/java/frc/robot/autonomous/ResetAuto.java
+++ src/main/java/frc/robot/autonomous/ResetAuto.java
@@ -1,2 +1,20 @@
-package frc.robot.autonomous;public class ResetAuto {
+package frc.robot.autonomous;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.gripper.Gripper;
+import frc.robot.subsystems.intake.commands.Retract;
+
+public class ResetAuto extends SequentialCommandGroup {
+
+    public ResetAuto() {
+        Gripper gripper = Gripper.getInstance();
+
+        addCommands(
+                new Retract(Retract.Mode.UP),
+                new InstantCommand(gripper::close, gripper),
+                new WaitCommand(0.02)
+        );
+    }
 }

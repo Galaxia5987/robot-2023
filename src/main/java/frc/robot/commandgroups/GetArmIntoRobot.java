e926b63e55ea5fba422479a0712004403df5b08b
GaiaZano05 <gaya.zano11@gmail.com>
2023-03-20 21:17:18 +0200

    Remove redandent commands in getArmIntoRobot

--- src/main/java/frc/robot/commandgroups/GetArmIntoRobot.java
+++ src/main/java/frc/robot/commandgroups/GetArmIntoRobot.java
@@ -1,13 +1,9 @@
 package frc.robot.commandgroups;
 
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.arm.commands.SetArmsPositionAngular;
-import frc.robot.subsystems.arm.commands.SetElbowAngle;
-import frc.robot.subsystems.arm.commands.SetShoulderAngle;
 
 public class GetArmIntoRobot extends SequentialCommandGroup {
 
@@ -15,13 +11,10 @@ public class GetArmIntoRobot extends SequentialCommandGroup {
         Arm arm = Arm.getInstance();
 
         addCommands(
-                new ConditionalCommand(
-                        new InstantCommand(),
-                        new SetArmsPositionAngular(() -> ArmConstants.IN_ROBOT1, 0.15).andThen(new SetArmsPositionAngular(() -> ArmConstants.IN_ROBOT2, 0.1)),
-                        arm::armIsInRobot
-                ),
-                new SetShoulderAngle(129).alongWith(new InstantCommand(arm::stop)),
-                new SetElbowAngle(329)
+                new SetArmsPositionAngular(() -> ArmConstants.IN_ROBOT1, 0.15).unless(arm::armIsInRobot),
+                new SetArmsPositionAngular(() -> ArmConstants.IN_ROBOT2, 0.1)
+//                new ArmAxisControl(1, 0, -0.02,
+//                        0, 0).until(() -> arm.getEndPosition().getY() <= -0.13)
         );
     }
 }

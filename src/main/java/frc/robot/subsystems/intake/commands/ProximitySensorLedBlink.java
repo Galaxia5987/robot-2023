db83ca1c2a2c691bed545cffb4231a9490b6fef3
GaiaZano05 <gaya.zano11@gmail.com>
2023-03-26 21:09:43 +0300

    Change time to 0.2 seconds

--- src/main/java/frc/robot/subsystems/intake/commands/ProximitySensorLedBlink.java
+++ src/main/java/frc/robot/subsystems/intake/commands/ProximitySensorLedBlink.java
@@ -24,6 +24,7 @@ public class ProximitySensorLedBlink extends CommandBase {
 
     @Override
     public void end(boolean interrupted) {
+        leds.setBlinkTime(0.2);
         leds.setBlink(false);
     }
 

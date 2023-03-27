c6fb1759b6131665f1ef9e16ee653ddf67421646
Emma <emmalivne.03@gmail.com>
2023-03-27 09:35:22 +0300

    Add LED constants

--- src/main/java/frc/robot/subsystems/leds/LedConstants.java
+++ src/main/java/frc/robot/subsystems/leds/LedConstants.java
@@ -1,5 +1,16 @@
 package frc.robot.subsystems.leds;
 
+import edu.wpi.first.wpilibj.util.Color;
+
 public final class LedConstants {
     public static final int LED_LENGTH = 19;
+
+    public static final Color kGalaxiaBlue = new Color(0, 156, 189);
+    public static final Color kPurple = Color.kPurple;
+    public static final Color kRed = Color.kRed;
+    public static final Color kGreen = Color.kGreen;
+
+    public static final double SLOW_BLINK_TIME = 0.3;
+    public static final double MID_BLINK_TIME = 0.25;
+    public static final double FAST_BLINK_TIME = 0.2;
 }

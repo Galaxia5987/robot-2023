2e09f15c93ab763fcbbbebc83704543bcd86ff8a
GaiaZano05 <gaya.zano11@gmail.com>
2023-03-27 19:51:20 +0300

    Try to add Led function to take cone

--- src/main/java/frc/robot/commandgroups/TakeCone.java
+++ src/main/java/frc/robot/commandgroups/TakeCone.java
@@ -1,12 +1,15 @@
 package frc.robot.commandgroups;
 
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.arm.commands.ArmAxisControl;
 import frc.robot.subsystems.gripper.Gripper;
 import frc.robot.subsystems.leds.LedConstants;
 import frc.robot.subsystems.leds.Leds;
+import frc.robot.subsystems.leds.command.Blink;
 
 public class TakeCone extends SequentialCommandGroup {
 
@@ -14,11 +17,10 @@ public class TakeCone extends SequentialCommandGroup {
         Gripper gripper = Gripper.getInstance();
         Leds leds = Leds.getInstance();
         addCommands(
+                new InstantCommand(() -> leds.setBlinkTime(LedConstants.FAST_BLINK_TIME)),
                 new ArmAxisControl(1, 0.02, 0)
                         .until(() -> gripper.getDistance() < ArmConstants.FEEDER_DISTANCE),
-                new InstantCommand(() -> leds.setBlinkTime(LedConstants.FAST_BLINK_TIME)),
-                new InstantCommand(() -> leds.setBlink(true, LedConstants.kGalaxiaBlue))
-                        .until(() -> !gripper.isOpen())
+                new InstantCommand(() ->leds.setBlink(true, LedConstants.kGalaxiaBlue))
         );
     }
 }

1f193d753fd1d3f1983ec1a24e48a5db79c21b2e
GaiaZano05 <gaya.zano11@gmail.com>
2023-03-28 21:13:04 +0300

    Fix rainbow LEDs

--- src/main/java/frc/robot/subsystems/leds/Leds.java
+++ src/main/java/frc/robot/subsystems/leds/Leds.java
@@ -15,10 +15,12 @@ public class Leds extends SubsystemBase {
     public AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(LedConstants.LED_LENGTH);
     private Color color = Color.kPurple;
     private Color secondaryColor = new Color(0, 0, 0);
-    private double blinkTime = 0;
+    private double blinkTime = LedConstants.MID_BLINK_TIME;
     private boolean blink = false;
+    private boolean isRainbow = false;
+    private int rainbowHue = 0;
 
-    private Mode mode = Mode.ON;
+    private Mode mode = Mode.PRIMARY_COLOR;
 
     private Leds() {
         leds.setLength(ledBuffer.getLength());
@@ -44,7 +46,7 @@ public class Leds extends SubsystemBase {
         color = Color.kPurple;
     }
 
-    public void setColor (Color color) {
+    public void setColor(Color color) {
         this.color = color;
     }
 
@@ -59,6 +61,22 @@ public class Leds extends SubsystemBase {
     public void setBlink(boolean blink, Color secondaryColor) {
         this.blink = blink;
         this.secondaryColor = secondaryColor;
+    }
+
+    public void setBlink(boolean blink, Color secondaryColor, double blinkTime) {
+        this.blink = blink;
+        this.secondaryColor = secondaryColor;
+        this.blinkTime = blinkTime;
+    }
+
+    public void toggleRainbow() {
+        if (!isRainbow) {
+            this.mode = Mode.ELSE;
+            this.isRainbow = true;
+        } else {
+            this.mode = Mode.PRIMARY_COLOR;
+            this.isRainbow = false;
+        }
     }
 
     public void toggle() {
@@ -77,31 +95,44 @@ public class Leds extends SubsystemBase {
     public void periodic() {
         if (blink) {
             if (timer.hasElapsed(blinkTime)) {
-                if (mode == Mode.ON) {
-                    mode = Mode.OFF;
+                if (mode == Mode.PRIMARY_COLOR) {
+                    mode = Mode.SECONDARY_COLOR;
                 } else {
-                    mode = Mode.ON;
+                    mode = Mode.PRIMARY_COLOR;
                 }
                 timer.reset();
             }
-        } else {
-            mode = Mode.ON;
+        } else if(mode != Mode.ELSE){
+            mode = Mode.PRIMARY_COLOR;
         }
 
-        if (mode == Mode.ON) {
+
+        if (mode == Mode.PRIMARY_COLOR) {
             for (int i = 0; i < ledBuffer.getLength(); i++) {
                 ledBuffer.setLED(i, color);
             }
             leds.setData(ledBuffer);
-        } else {
+        } else if (mode == Mode.SECONDARY_COLOR) {
             for (int i = 0; i < ledBuffer.getLength(); i++) {
                 ledBuffer.setLED(i, secondaryColor);
             }
             leds.setData(ledBuffer);
         }
+        if (isRainbow){
+            mode = Mode.ELSE;
+            System.out.println("before loop statement");
+            for (int i = 0; i < ledBuffer.getLength(); i++) {
+                ledBuffer.setHSV(i, rainbowHue, 255, 180);
+                rainbowHue += (180 / ledBuffer.getLength());
+                leds.setData(ledBuffer);
+                System.out.println("in-loop check statement");
+            rainbowHue%=180;
+            }
+            System.out.println("after loop statement");
+        }
     }
 
     private enum Mode {
-        OFF, ON
+        SECONDARY_COLOR, PRIMARY_COLOR, ELSE
     }
 }

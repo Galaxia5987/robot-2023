4361b1438120114ec1ec374a0ea5b4073322874e
GaiaZano05 <gaya.zano11@gmail.com>
2023-03-29 13:34:04 +0300

    Make one default command for blinking when holding a cone or when cube inside robot!!

--- src/main/java/frc/robot/subsystems/intake/commands/ProximitySensorDefualtCommand.java
+++ src/main/java/frc/robot/subsystems/intake/commands/ProximitySensorDefualtCommand.java
@@ -1,36 +1,30 @@
 package frc.robot.subsystems.intake.commands;
 
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.arm.ArmConstants;
+import frc.robot.subsystems.gripper.Gripper;
 import frc.robot.subsystems.intake.ProximitySensor;
 import frc.robot.subsystems.leds.LedConstants;
 import frc.robot.subsystems.leds.Leds;
 
 
-public class ProximitySensorLedBlink extends CommandBase {
+public class ProximitySensorDefualtCommand extends CommandBase {
     private final ProximitySensor proximitySensor = ProximitySensor.getInstance();
     private final Leds leds = Leds.getInstance();
+    private final Gripper gripper = Gripper.getInstance();
 
     private boolean ProximitySensorState = proximitySensor.isBeamBlocked();
 
-    public ProximitySensorLedBlink() {
+    public ProximitySensorDefualtCommand() {
         this.ProximitySensorState = proximitySensor.isBeamBlocked();
-        addRequirements(proximitySensor);
+        addRequirements(proximitySensor, leds);
     }
 
     @Override
     public void execute() {
-        leds.setBlink(true);
+        if (proximitySensor.isBeamBlocked() || (gripper.getDistance() < ArmConstants.FEEDER_DISTANCE && gripper.isOpen()))
+            leds.setBlink(true, LedConstants.kGalaxiaBlue, LedConstants.FAST_BLINK_TIME);
+        else leds.setBlink(false);
     }
 
-    @Override
-    public void end(boolean interrupted) {
-        leds.setBlinkTime(LedConstants.FAST_BLINK_TIME);
-        leds.setBlink(false, LedConstants.kGalaxiaBlue);
-    }
-
-    @Override
-    public boolean isFinished() {
-        return !proximitySensor.isBeamBlocked();
-    }
 }

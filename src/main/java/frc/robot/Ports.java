f451e20ad94a3624302a66cc306e77d40bad7a7d
Emma <emmalivne.03@gmail.com>
2023-07-23 23:24:22 +0300

    Update ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -12,19 +12,39 @@ public final class Ports {
     }
 
     public static final class SwerveDrive {
-        public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR_ID = 1;
-        public static final int FRONT_LEFT_MODULE_STEER_MOTOR_ID = 6;
-        public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR_ID = 3;
-        public static final int FRONT_RIGHT_MODULE_STEER_MOTOR_ID = 4;
-        public static final int REAR_LEFT_MODULE_DRIVE_MOTOR_ID = 7;
-        public static final int REAR_LEFT_MODULE_STEER_MOTOR_ID = 8;
-        public static final int REAR_RIGHT_MODULE_DRIVE_MOTOR_ID = 5;
-        public static final int REAR_RIGHT_MODULE_STEER_MOTOR_ID = 2;
+        public static final int FRONT_LEFT_DRIVE_MOTOR_ID = 1;
+        public static final int FRONT_LEFT_ANGLE_MOTOR_ID = 6;
+        public static final int FRONT_RIGHT_DRIVE_MOTOR_ID = 3;
+        public static final int FRONT_RIGHT_ANGLE_MOTOR_ID = 4;
+        public static final int REAR_LEFT_DRIVE_MOTOR_ID = 7;
+        public static final int REAR_LEFT_ANGLE_MOTOR_ID = 8;
+        public static final int REAR_RIGHT_DRIVE_MOTOR_ID = 5;
+        public static final int REAR_RIGHT_ANGLE_MOTOR_ID = 2;
+
+        public static final int[] DRIVE_IDS = {
+                FRONT_LEFT_DRIVE_MOTOR_ID,
+                FRONT_RIGHT_DRIVE_MOTOR_ID,
+                REAR_LEFT_DRIVE_MOTOR_ID,
+                REAR_RIGHT_DRIVE_MOTOR_ID
+        };
+        public static final int[] ANGLE_IDS = {
+                FRONT_LEFT_ANGLE_MOTOR_ID,
+                FRONT_RIGHT_ANGLE_MOTOR_ID,
+                REAR_LEFT_ANGLE_MOTOR_ID,
+                REAR_RIGHT_ANGLE_MOTOR_ID
+        };
 
         public static final int FRONT_LEFT_ENCODER_ID = 5;
         public static final int FRONT_RIGHT_ENCODER_ID = 8;
         public static final int REAR_LEFT_ENCODER_ID = 2;
         public static final int REAR_RIGHT_ENCODER_ID = 7;
+
+        public static final int[] ENCODER_IDS = {
+                FRONT_LEFT_ENCODER_ID,
+                FRONT_RIGHT_ENCODER_ID,
+                REAR_LEFT_ENCODER_ID,
+                REAR_RIGHT_ENCODER_ID
+        };
 
         public static boolean FRONT_LEFT_DRIVE_INVERTED = true;
         public static boolean FRONT_LEFT_ANGLE_INVERTED = true;
@@ -34,6 +54,19 @@ public final class Ports {
         public static boolean REAR_LEFT_ANGLE_INVERTED = true;
         public static boolean REAR_RIGHT_DRIVE_INVERTED = true;
         public static boolean REAR_RIGHT_ANGLE_INVERTED = true;
+
+        public static final boolean[] DRIVE_INVERTED = {
+                FRONT_LEFT_DRIVE_INVERTED,
+                FRONT_RIGHT_DRIVE_INVERTED,
+                REAR_LEFT_DRIVE_INVERTED,
+                REAR_RIGHT_DRIVE_INVERTED
+        };
+        public static final boolean[] ANGLE_INVERTED = {
+                FRONT_LEFT_ANGLE_INVERTED,
+                FRONT_RIGHT_ANGLE_INVERTED,
+                REAR_LEFT_ANGLE_INVERTED,
+                REAR_RIGHT_ANGLE_INVERTED
+        };
     }
 
     public static class Leds {

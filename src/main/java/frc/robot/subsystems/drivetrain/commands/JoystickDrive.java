6eec7fd656625dfd52f4258846a387af11c16ad6
Emma <emmalivne.03@gmail.com>
2023-07-23 23:22:39 +0300

    Add JoystickDrive command

--- src/main/java/frc/robot/subsystems/drivetrain/commands/JoystickDrive.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/JoystickDrive.java
@@ -1,56 +1,28 @@
 package frc.robot.subsystems.drivetrain.commands;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Ports;
-import frc.robot.subsystems.drivetrain.DriveSignal;
-import frc.robot.subsystems.drivetrain.SwerveConstants;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
-import frc.robot.subsystems.gyroscope.Gyroscope;
-import frc.robot.subsystems.vision.Limelight;
 
 public class JoystickDrive extends CommandBase {
-    private final SwerveDrive swerveDrive = SwerveDrive.getInstance();
-    private final Limelight limelight = Limelight.getInstance();
-    private final Gyroscope gyroscope = Gyroscope.getInstance();
+    private final SwerveDrive swerveDrive;
+    private final Joystick joystick1;
+    private final Joystick joystick2;
 
-    private final Joystick leftJoystick;
-    private final Joystick rightJoystick;
-
-    public JoystickDrive(Joystick leftJoystick, Joystick rightJoystick) {
-        this.leftJoystick = leftJoystick;
-        this.rightJoystick = rightJoystick;
+    public JoystickDrive(SwerveDrive swerveDrive, Joystick joystick1, Joystick joystick2) {
+        this.swerveDrive = swerveDrive;
+        this.joystick1 = joystick1;
+        this.joystick2 = joystick2;
         addRequirements(swerveDrive);
     }
 
     @Override
     public void execute() {
-        if (leftJoystick.getRawButton(Ports.UI.JOYSTICK_TOP_BOTTOM_BUTTON)) {
-            swerveDrive.lock();
-        } else {
-            double vx = -leftJoystick.getY();
-            double vy = -leftJoystick.getX();
-            double omega = -rightJoystick.getX();
-
-            double magnitude = Math.hypot(vx, vy);
-            double angle = Math.atan2(vy, vx);
-            magnitude = MathUtil.applyDeadband(magnitude, 0.05);
-            vx = Math.cos(angle) * magnitude;
-            vy = Math.sin(angle) * magnitude;
-            omega = MathUtil.applyDeadband(omega, 0.05);
-
-            vx = Math.copySign(vx * vx, vx);
-            vy = Math.copySign(vy * vy, vy);
-
-            DriveSignal signal = new DriveSignal(
-                    vx * SwerveConstants.MAX_VELOCITY_AUTO,
-                    vy * SwerveConstants.MAX_VELOCITY_AUTO,
-                    0.7 * omega * SwerveConstants.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
-                    new Translation2d(),
-                    true);
-            swerveDrive.drive(signal);
-        }
+        swerveDrive.drive(
+                joystick1.getY(),
+                joystick1.getX(),
+                joystick2.getX(),
+                true
+        );
     }
 }
